220c37bc8bd92c56562acb6f05c933705d2772dc
Paulo Freire <paulgarden95@gmail.com>
2022-05-26 23:38:46 -0300

    chore: Refactoring the code into React components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Gabi from './Gabi';
+import Head from './components/Head';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Gabi />
+    <Head />
   </React.StrictMode>
 );
 
