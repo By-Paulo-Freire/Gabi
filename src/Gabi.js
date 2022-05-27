fc86dba1d7aa38518e8c5a78e41a2c80de0abd05
Paulo Freire <paulgarden95@gmail.com>
2022-05-27 11:02:33 -0300

    chore: Deleting obsolete code/file structures

--- src/Gabi.js
+++ src/Gabi.js
@@ -6,7 +6,7 @@ class Gabi extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          emotion: 'teste',
+          emotion: 'happy',
         };
       }
     render () {
