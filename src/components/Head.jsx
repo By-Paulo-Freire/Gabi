924a5a24ad880a84a8f72d2bd039205b5e01ee80
Paulo Freire <paulgarden95@gmail.com>
2022-05-26 22:48:59 -0300

    chore: Creating a public/index.hmtl, laying out some other files for future work

---
+++ src/components/Head.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './head.css';
+
+ReactDOM.render(
+  <div class="gabi">
+            <div class="bg-hair">
+              <div class="head">
+                <div class="eye radius ha hb"></div>
+                <div class="eye radius ha hb"></div>
+                <div class="nose"></div>
+                <div class="eyebrow"></div>
+                <div class="eyebrow"></div>
+                <div class="mouth radius"></div>
+                <div class="fg-hair"></div>
+              </div>
+              </div>
+            </div>,
+  document.getElementById('root')
+);
