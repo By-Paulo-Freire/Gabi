5b300b0a5a36744aae2a4e95f04aee09320da835
Paulo Freire <paulgarden95@gmail.com>
2022-05-27 10:25:00 -0300

    feat: Created component Gabi

--- src/Gabi.js
+++ src/Gabi.js
@@ -1,3 +1,19 @@
 import React from 'react';
+import Head from './components/Head';
 
 
+class Gabi extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          emotion: 'teste',
+        };
+      }
+    render () {
+        return (
+            <Head />
+        )
+    }
+}
+
+export default Gabi;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Head from './components/Head';
+import Gabi from './Gabi';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Head />
+    <Gabi />
   </React.StrictMode>
 );
 
